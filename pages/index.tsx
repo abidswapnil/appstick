510c59a068ca904d27978e0c2d18cf4ed07904ee
abid <swapnil1510@cseku.ac.bd>
2023-03-23 16:56:28 +0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div>Hello folks, this is my first nextJS project.</div>
+        <div>Hello folks, this is a appstick task.</div>
       </main>
     </>
   )
