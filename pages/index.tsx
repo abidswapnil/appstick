648a439f60c4a5dc73179e8eada316a71eb9cdea
abid <swapnil1510@cseku.ac.bd>
2023-03-23 15:43:04 +0600

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
-            <div>Hello next js</div>
           </p>
           <div>
             <a
