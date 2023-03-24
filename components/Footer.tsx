9f0a28d924b43b51b36dd4ba13318cc62ff5c017
abid <swapnil1510@cseku.ac.bd>
2023-03-25 05:37:26 +0600

    7th commit - 5:37am

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import styles from '@/styles/DivSpace.module.css'
 import OvalButton from './Buttons/OvalButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
@@ -17,11 +19,50 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <ul className={styles.ul}>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
+          <ul className={styles.rightfooterUl}>
+            <li>
+              <ul className={styles.rightfooternestedUl}>
+                <span>Banking</span>
+                <a href=""><li>Add new accounts</li></a>
+                <a href=""><li>My account</li></a>
+                <a href=""><li>My savings</li></a>
+                <a href=""><li>Add new cards</li></a>
+                <a href=""><li>My cards</li></a>
+              </ul>
+            </li>
+            <li>
+              <ul className={styles.rightfooternestedUl}>
+                <span>Insurence</span>
+                <a href=""><li>Home insurence</li></a>
+                <a href=""><li>Mobility insurence</li></a>
+                <a href=""><li>Travel Insurence</li></a>
+              </ul>
+            </li>
+            <li>
+              <ul className={styles.rightfooternestedUl}>
+                <span>Traveling</span>
+                <a href=""><li>Add new booking</li></a>
+                <a href=""><li>My books</li></a>
+                <a href=""><li>Plan & roots</li></a>
+              </ul>
+            </li>
+            <li>
+              <ul className={styles.rightfooternestedUl}>
+                <span>More</span>
+                <a href=""><li>About us</li></a>
+                <a href=""><li>Send email</li></a>
+                <a href=""><li>Blog</li></a>
+                <a href=""><li>FAQ</li></a>
+                <a href=""><li>Service policy</li></a>
+              </ul>
+            </li>
           </ul>
+          <div className={styles.socialLinks}>
+            <a href="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://www.linkedin.com/"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://www.youtube.com/"><FontAwesomeIcon icon={faYoutube} /></a>
+          </div>
         </div>
       </div>
     )
